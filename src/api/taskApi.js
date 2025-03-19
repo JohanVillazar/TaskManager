5cf7a38f8f9b2057b2cc7ffe4a3c75016561b76e
unknown <villazarjohan@gmail.com>
2025-03-18 20:21:47 -0500

    tasksfront1.1.1

--- src/api/taskApi.js
+++ src/api/taskApi.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/tasks?all=true";
+const API_URL = "https://tasksmanagerback.onrender.com";
 
 export const fetchTasks = async () => {
     try {
--- src/components/KanbanBoard.jsx
+++ src/components/KanbanBoard.jsx
@@ -21,7 +21,7 @@ function KanbanBoard() {
     const fetchTasks = async () => {
       try {
         const token = localStorage.getItem("token");
-        const response = await fetch("http://localhost:3000/api/tasks", {
+        const response = await fetch("https://tasksmanagerback.onrender.com/tasks", {
           headers: {
             Authorization: `Bearer ${token}`,
           }
@@ -72,7 +72,7 @@ function KanbanBoard() {
     try {
       const token = localStorage.getItem("token");
   
-      await fetch(`http://localhost:3000/tasks/${active.id}/status`, {
+      await fetch(`https://tasksmanagerback.onrender.com/tasks/${active.id}/status`, {
         method: "PATCH",
         headers: {
           "Content-Type": "application/json",
